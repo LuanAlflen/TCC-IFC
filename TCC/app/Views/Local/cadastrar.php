42d42589d3d42eb63de4fada5a8703e59e7ab23c
LuanAlflen <luan.alflen4@gmail.com>
2018-06-10 23:50:10 -0300

    Add files via upload

--- TCC/app/Views/Local/cadastrar.php
+++ TCC/app/Views/Local/cadastrar.php
@@ -4,6 +4,61 @@
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="../../assets/css/main.css">
 	<title>Formulario</title>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript">
+        //ao carregar a pagina, fica sempre pronto pra executar
+        $(function(){
+
+            ///////////////////AQUI COMEÇA O CARRO 1////////////////////////////
+
+            $('#estados').change(function(){
+                if( $(this).val() ) {
+                    $('#municipios').hide();
+                    $('.carregando').show();
+
+
+                    //http://localhost/tcc/app/controllers/controladorTab.php?marca=$(this).val()
+
+                    $.getJSON('https://servicodados.ibge.gov.br/api/v1/localidades/estados/'+$(this).val()+'.json', function(j){
+                        var options = '<option value="">Selecione...</option>';
+                        for (var i = 0; i < j.length; i++) {
+                            options += '<option value="' +
+                                j[i].id + '">' +
+                                j[i].nome + '</option>';
+                        }
+                        $('#municipios').html(options).show();
+                        $('.carregando').hide();
+                    });
+                } else {
+                    $('#municipios').html(
+                        '<option value="">-- Selecione um estado --</option>'
+                    );
+                }
+            });
+
+            $('#municipios').change(function(){
+                if( $(this).val() ) {
+                    $('.carregando').show();
+                    $.getJSON(
+                        'https://servicodados.ibge.gov.br/api/v1/localidades/municipios'+$('#estados').val()+'/'+$(this).val()+'.json', function(j){
+                            var options = '<option value="">Selecione...</option>';
+                            for (var i = 0; i < j.length; i++) {
+                                options += '<option value="' +
+                                    j[i].id + '">' +
+                                    j[i].nome + '</option>';
+                            }
+                            $('.carregando').hide();
+                        });
+                } else {
+                    $('#carregando').html(
+                        '<option value="">-- Selecione um municipio --</option>'
+                    );
+                }
+            });
+        })
+
+    </script>
+
 </head>
 <body>
 
@@ -17,14 +72,45 @@
 			<input class="input" type="file"  name="foto">
 			<input class="input" type="text"  name="nome"     placeholder="Nome"     required>
             <input class="input" type="email" name="email"    placeholder="Email"    required>
-            <input class="input" type="text"  name="endereco" placeholder="Endereço" required>
             <input class="input" type="text"  name="telefone" placeholder="Telefone" required>
-            <textarea rows="5" cols="40" maxlength="500" name="descricao" placeholder="Descrição..."></textarea>
-                <select name="categoria" class="form-control"">
-                    <?php foreach ($categorias as $categoria):?>
-                        <option><?= $categoria->nome ?></option>
-                    <?php endforeach ?>
+            <br><textarea rows="5" cols="40" maxlength="500" name="descricao" placeholder="Descrição..."></textarea>
+            <p>Categoria:</p>
+            <select name="categoria" class="select">
+                <option value="0">Selecione...</option>
+            <?php foreach ($categorias as $categoria):?>
+                <option><?= $categoria->nome ?></option>
+            <?php endforeach ?>
+            </select>
+            <p>Estados:</p>
+<!--            Aqui começa o endereco(Estados e municipios)-->
+            <?php
+            $url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'; // marcas
+
+            //http://fipeapi.appspot.com/api/1/carros/veiculos/21.json // veiculos da marca 21
+
+            $data = file_get_contents($url); // put the contents of the file into a variable
+            $estados = json_decode($data); // decode the JSON feed
+            echo '<select name="estados" class="select" id="estados" >';
+            echo '<option selected>Selecione...</option>';
+
+            foreach ($estados as $estado) {
+
+                echo '<option value="'.$estado->id.'">'.$estado->nome.'</option>';
+
+            }
+
+            echo '</select>';
+            ?>
+            <p>Municipios:</p>
+
+                <select name="municipios" class="select" id="municipios">
+                    <option>Selecione...</option>
                 </select>
+
+            <p>Endereco:</p>
+
+                <input class="input" type="text"  name="endereco" placeholder="Endereço" required>
+
             <input class="input" value="<?= $_GET['id'] ?>" type="hidden" name="iduser" placeholder="Id_user" required>
 			<div class="btn__form">
             	<input class="btn__submit" type="reset" value="Limpar">
