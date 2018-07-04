9220905d9daaf02cc16150ed956b24572a4a75f0
LuanAlflen <luan.alflen4@gmail.com>
2018-07-04 16:17:12 -0300

    Add files via upload

--- TCC/app/Views/Usuario/cadastrar.php
+++ TCC/app/Views/Usuario/cadastrar.php
@@ -6,6 +6,47 @@
 
 	<title>Formulario</title>
 
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>
+        function TestaCPF() {
+            $(".erroCPF").hide();
+
+            var strCPF = $("#cpf").val();
+            var Soma;
+            var Resto;
+            Soma = 0;
+            if (strCPF == "00000000000"){
+                $(".erroCPF").show();
+                return false;
+            }
+
+            for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+            Resto = (Soma * 10) % 11;
+
+            if ((Resto == 10) || (Resto == 11))  Resto = 0;
+            if (Resto != parseInt(strCPF.substring(9, 10)) ) {
+                $(".erroCPF").show();
+                return false;
+            }
+
+            Soma = 0;
+            for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+            Resto = (Soma * 10) % 11;
+
+            if ((Resto == 10) || (Resto == 11))  Resto = 0;
+            if (Resto != parseInt(strCPF.substring(10, 11) ) ) {
+                $(".erroCPF").show();
+                return false;
+            }
+            return true;
+        }
+
+        $(document).ready(function (){
+            $(".erroCPF").hide();
+            
+        });
+    </script>
+
 </head>
 <body>
 
@@ -18,19 +59,20 @@
 		</div>
 
 
-		<form class="form__reg"  method="post"   action="?acao=cadastrar" enctype="multipart/form-data">
+		<form class="form__reg"  method="post"   action="?acao=cadastrar" enctype="multipart/form-data" onsubmit="return TestaCPF();">
 			<input class="input" type="file"     name="foto">
 			<input class="input" type="text"     name="nome"     placeholder="Nome"     required>
 			<input class="input" type="text"     name="login"    placeholder="Login"     required>
 			<input class="input" type="password" name="senha"    placeholder="Senha"    required>
             <input class="input" type="email"    name="email"    placeholder="Email"    required>
             <input class="input" type="text"     name="telefone" placeholder="Telefone">
-			<input class="input" type="text"     name="cpf"      placeholder="CPF"      required>
+			<input id="cpf" class="input" type="number"     name="cpf"      placeholder="CPF"      required>
+            <div class="erroCPF" style="color: red">Informe um CPF válido</div>
             <input class="input" type="hidden"   name="tipuser" required>
-			<?php
+            <?php
             if (@$_GET['erro'] == 1){?>
-			<div class="error-text" style="color: red">Este login ja existe, tente novamente.</div>
-			<?php } ?>
+                <div class="error-text" style="color: red">Este login ja existe, tente novamente.</div>
+            <?php } ?>
             <div class="btn__form">
 				<input class="btn__reset" type="reset" value="Limpar">
 			 	<input class="btn__submit" type="submit" name="gravar" value="Salvar" >
