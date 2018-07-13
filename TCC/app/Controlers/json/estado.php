39351858c706f0a403bf00c962e290e6da548934
LuanAlflen <luan.alflen4@gmail.com>
2018-07-13 00:47:06 -0300

    Add files via upload

--- TCC/app/Controlers/json/estado.php
+++ TCC/app/Controlers/json/estado.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aluno
- * Date: 12/07/18
- * Time: 15:49
- */
+
+// Conectando
+require_once '../../Models/DBConnection.php';
+
+$con = new DBConnection();
+$res = $con->getConexao();
+
+// Executando consulta SQL
+//$query = 'SELECT * FROM montadora WHERE idmontadora =5';
+//$resultado = $res->query($query)->fetchAll(PDO::FETCH_ASSOC);
+//$json = json_encode($resultado, JSON_PRETTY_PRINT);
+//echo "$json";
+
+$url = file_get_contents('https://servicodados.ibge.gov.br/api/v1/localidades/estados/');
+$estados = json_decode($url);
+
+foreach ($estados as  $estado){
+    echo "insert into estados (id, sigla, nome) values ('".$estado->id."','".$estado->sigla."','".$estado->nome."');<br>";
+}
--- TCC/app/Controlers/json/municipio.php
+++ TCC/app/Controlers/json/municipio.php
@@ -1,7 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aluno
- * Date: 12/07/18
- * Time: 15:49
- */
+
+// Conectando
+require_once '../../Models/DBConnection.php';
+
+$con = new DBConnection();
+$res = $con->getConexao();
+
+// Executando consulta SQL
+
+//$query = 'SELECT * FROM modelo WHERE idmodelo =5';
+//$resultado = $res->query($query)->fetchAll(PDO::FETCH_ASSOC);
+//$json = json_encode($resultado, JSON_PRETTY_PRINT);
+//echo "$json";
+
+// Consulta SQL e Pesquisa na tabela FIPE para cada modelo em montadora
+
+$query = 'SELECT id_estado FROM estados ';
+$resultado = $res->query($query)->fetchAll(PDO::FETCH_ASSOC);
+//print_r($resultado);
+
+foreach ($resultado as $rst) {
+    //echo 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/'.$rst['id_estado'].'/muncipios';
+    $url = file_get_contents('https://servicodados.ibge.gov.br/api/v1/localidades/estados/'.$rst['id_estado'].'/municipios');
+
+    $municipios = json_decode($url);
+
+    foreach ($municipios as  $municipio){
+        echo "insert into municipios (id_municipio, nome, id_estado) values ('".$municipio->id."', '".$municipio->nome."', ".$rst['id_estado'].");<br>";
+    }
+
+
+}
+
+// Consulta PHP e criação de pastas para cada marca
+
+//$query = 'SELECT montadora FROM montadora';
+//$resultado = $res->query($query)->fetchAll(PDO::FETCH_ASSOC);
+//print_r($resultado);
+//foreach ($resultado as $rst) {
+
+//}
+//NOSSO BANCO TEM 5545 MUNICIPIOS, DEVERIA TER 5571, EU PROVAVELMENTE PERDI ALGUNS DO PARANA OU DE SAO PAULO, BOA SORTE BRYAN, QUE OS JOGOS COMECEM
